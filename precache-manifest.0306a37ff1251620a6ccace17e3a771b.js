8b055a497da792654c16805f4a58c4adfc0bfaa4
Jeann Carlos Batemarque <jeann.carlos4@gmail.com>
2020-06-24 15:40:26 -0300

    Updates

---
+++ precache-manifest.0306a37ff1251620a6ccace17e3a771b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f96bc425cb35c9643e8f21598dd5675",
+    "url": "/UnStade-Landing/index.html"
+  },
+  {
+    "revision": "dfe2bd90b32c56e7710e",
+    "url": "/UnStade-Landing/static/js/2.5be1bb7e.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/UnStade-Landing/static/js/2.5be1bb7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ffe302a97148554716",
+    "url": "/UnStade-Landing/static/js/main.902d10ad.chunk.js"
+  },
+  {
+    "revision": "83a2ce75c3b783b46fba",
+    "url": "/UnStade-Landing/static/js/runtime-main.30e00bae.js"
+  },
+  {
+    "revision": "a05c6769bd29c80c437ef01ea2d6ef01",
+    "url": "/UnStade-Landing/static/media/Logo.a05c6769.svg"
+  },
+  {
+    "revision": "d070f8f2dd3331efd1e7f397bc899ae7",
+    "url": "/UnStade-Landing/static/media/intro.d070f8f2.gif"
+  }
+]);
